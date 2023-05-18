0589d75958f5f8eca936ed8893e3aafb7066f6ff
FilipNedovic <filip.nedovic@epam.com>
2023-05-18 11:44:50 +0200

    implemented custom command for login via BE

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,18 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("loginBE", () => {
+  cy.request({
+    method: "POST",
+    url: `${Cypress.env("apiUrl")}/login`,
+    body: {
+      email: Cypress.env("email"),
+      password: Cypress.env("password"),
+    },
+  }).then((response) => {
+    window.localStorage.setItem("token", response.body.token);
+    window.localStorage.setItem("user", JSON.stringify(response.body.user));
+    window.localStorage.setItem("user_id", response.body.user.id);
+  });
+});
