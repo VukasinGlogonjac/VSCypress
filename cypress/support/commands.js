57bf380e66169ffd556d8adb73bb3fd7e7c6d6cf
unknown <vuleglogonjac@gmail.com>
2023-05-22 15:54:41 +0200

    added custom commands for creating etities via BE

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,6 +24,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+
 Cypress.Commands.add("loginBE", () => {
   cy.request({
     method: "POST",
@@ -38,3 +39,81 @@ Cypress.Commands.add("loginBE", () => {
     window.localStorage.setItem("user_id", response.body.user.id);
   });
 });
+
+
+Cypress.Commands.add("createOrganization", (orgName) => {
+  cy.request({
+    method: "POST",
+    url: "https://cypress-api.vivifyscrum-stage.com/api/v2/organizations",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    body: {
+      name: orgName,
+    },
+  }).then((response) => {
+    window.localStorage.setItem("orgId", response.body.id);
+  });
+});
+
+Cypress.Commands.add("createScrumBoard", (boardName, orgId) => {
+  cy.request({
+    method: "POST",
+    url: "https://cypress-api.vivifyscrum-stage.com/api/v2/boards",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    body: {
+      name: boardName,
+      type: "scrum_board",
+      organization_id: orgId,
+    },
+  }).then((response) => {
+    console.log('ID', response.body)
+    window.localStorage.setItem("boardId", response.body.id);
+  });
+});
+
+Cypress.Commands.add("createTask", (body) => {
+  cy.request({
+    method: "POST",
+    url: "https://cypress-api.vivifyscrum-stage.com/api/v2/tasks",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    body: body
+  }).then((response) => {
+    console.log(response.body.code, "CODEEEEE")
+    window.localStorage.setItem("taskCode", response.body.code);
+  });
+});
+
+Cypress.Commands.add("deleteOrganization", (orgId) => {
+  cy.request({
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    url: `https://cypress-api.vivifyscrum-stage.com/api/v2/organizations/${orgId}`,
+    body: {
+      passwordOrEmail: "test1234",
+    },
+  });
+});
+
+Cypress.Commands.add("deleteBoard", (boardId) => {
+  cy.request({
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    url: `https://cypress-api.vivifyscrum-stage.com/api/v2/boards/${boardId}`,
+  });
+});
+
+
+
+
+
+
+
