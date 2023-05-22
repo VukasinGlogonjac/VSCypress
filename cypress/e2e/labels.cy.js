39293a052dd3410db4142c8e54d247e6ca5f9822
unknown <vuleglogonjac@gmail.com>
2023-05-22 15:54:54 +0200

    tests

--- cypress/e2e/labels.cy.js
+++ cypress/e2e/labels.cy.js
@@ -1,57 +1,29 @@
 /// <reference types="Cypress" />
 import "cypress-real-events/support";
 import { labels } from "../page_object/labelsPOM";
-import { login } from "../page_object/loginPOM";
-const { email, password, apiUrlTask, apiUrlBoard } = Cypress.env()
-let taskId
-let boardId
-describe('Labels', () => {
-    beforeEach(() => {
-        login.login(email, password)
-    })
+import { storyTask } from "../fixtures/testData";
 
-    it('Create, assert and delete labels', () => {
-        cy.intercept({
-            method: "POST",
-            url: apiUrlBoard,
-        }).as("boardCreated");
-        labels.addNewBtn.realHover()
-        labels.addBoard.click()
-        labels.nameInputField.type("New Board")
-        labels.nextBtn.click()
-        labels.scrumBtn.click()
-        labels.nextBtn.click()
-        labels.nextBtn.click()
-        labels.nextBtn.click()
-        labels.nextBtn.click()
-        cy.wait('@boardCreated').then((intercept) => {
-            boardId = intercept.response.body.id
-            cy.log(boardId)
-            labels.findMyBoardId(boardId).click()
-        })
-        cy.intercept({
-            method: "POST",
-            url: apiUrlTask
-        }).as("taskCreated");
-        labels.trigerPlace.realHover()
-        labels.newTaskBtn.click()
-        labels.textArea.type("New Task")
-        labels.newTaskSave.click()
-        cy.wait('@taskCreated').then((intercept) => {
-            taskId = intercept.response.body.id
-            cy.log(taskId)
-            labels.findMyTaskId(taskId).click()
-        
-        })
-        labels.manageLabelBtn.click()
-        labels.labelTitle.type("New Label")
-        labels.labelColor.click()
-        labels.chooseLabelColor.clear()
-        labels.chooseLabelColor.type("#6bb9e8")
-        labels.confirmClrBtn.click()
-        labels.closeBtn.click()
-        labels.allLabels.click()
-        labels.deleteLabelBtn.click()
-        labels.yesBtn.realClick()
+let boardId;
+let taskCode;
+
+describe('Create, assert and delete labels', () => {
+    before(() => {
+        cy.loginBE();
+        cy.visit('/');
+        cy.contains('My Organizations').should('be.visible');
+        cy.createOrganization('test org').then(() => {
+            expect(window.localStorage.getItem('orgId')).to.exist;
+            cy.createScrumBoard('Test', window.localStorage.getItem('orgId')).then(() => {
+                cy.createTask(storyTask('created BEEEEEE', window.localStorage.getItem('boardId')))
+                boardId = window.localStorage.getItem('boardId');
+            })
+            taskCode = window.localStorage.getItem('taskCode');
+        });
+      
+    })
+    it('Create, assert and delete label successfully', () => {
+        cy.log("TASK CODE", )
+        cy.visit(`https://cypress.vivifyscrum-stage.com/boards/${boardId}/${window.localStorage.getItem('taskCode')}`)
+        labels.labelSucces()
     })
 })
