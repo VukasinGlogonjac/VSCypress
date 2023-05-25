5960152f7a94c1efe5ae76c46cd8bf39ba04d5a6
unknown <vuleglogonjac@gmail.com>
2023-05-25 12:22:09 +0200

    Create organization via FE done

---
+++ cypress/e2e/createOrgFe.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+import { createOrgFe } from "../page_object/createOrgFePOM";
+
+describe('Creating org via frontend', () =>{
+    beforeEach(() => {
+        cy.loginBE();
+        cy.visit('/');
+    })
+
+    it('Successfully created Organization',() => {
+        createOrgFe.createOrganization()
+    })
+
+    
+
+
+})
