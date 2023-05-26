f1d370a6216a40dbb28415d46253908b8f70eb00
unknown <vuleglogonjac@gmail.com>
2023-05-26 09:37:29 +0200

    Napravljene izmene sa pull request

---
+++ cypress/e2e/createOrgFe.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+import { createOrgFe } from "../page_object/createOrgFePOM";
+
+describe('Creating org via frontend', () =>{
+    beforeEach(() => {
+        cy.loginBE();
+        cy.visit('/');
+    })
+
+    it('Successfully created Organization',() => {
+        createOrgFe.createOrganization()
+    })
+
+})
