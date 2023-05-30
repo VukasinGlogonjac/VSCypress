ff991e811baa0e9703c9b63e376172c6c3303f08
FilipNedovic <filip.nedovic@epam.com>
2023-05-30 13:47:20 +0200

    date manipulation

--- cypress/page_object/datePickerPOM.js
+++ cypress/page_object/datePickerPOM.js
@@ -6,123 +6,132 @@ const currentMonth = Object.values(MONTHS)[date.getMonth()];
 const yesterday = date.getDate() - 1;
 
 class DatePicker {
-    setDate() {
-        let fiveDaysFromNow = new Date();
-        fiveDaysFromNow.setDate(fiveDaysFromNow.getDate() + 5)
+  get taskBodyTabs() {
+    return cy
+      .get('div[class="vs-c-modal__body-right el-col el-col-10"]')
+      .find(".el-tabs");
+  }
 
-        return fiveDaysFromNow;
+  get taskWorklog() {
+    return this.taskBodyTabs.find(".el-tabs__item").eq(1);
+  }
+
+  get manageWorklog() {
+    return cy.get('button[title="Manage Worklog"]');
+  }
+
+  get manageDateIcon() {
+    return cy.get('i[class="el-input__icon el-icon-date"');
+  }
+
+  get datepicker() {
+    return cy.get('div[class="el-picker-panel__content"]').find("tbody").eq(0);
+  }
+
+  get datepickerHeader() {
+    return cy.get(".el-date-picker__header");
+  }
+
+  get dateInputField() {
+    return cy.get('input[placeholder="Start date"]');
+  }
+
+  getFutureDate = (daysFromNow) => {
+    let targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + daysFromNow);
+
+    let dd = targetDate.getDate();
+    let mm = targetDate.getMonth();
+    let yyyy = targetDate.getFullYear();
+
+    if (dd < 10) {
+      dd = `0${dd}`;
     }
 
-    futureDate;
-    futureMonth
+    let dateString = `${dd} ${Object.values(MONTHS)[mm]} ${yyyy}`;
 
-    getFutureMonth() {
-        this.futureMonth = Object.values(MONTHS)[this.setDate().getMonth()];
+    return dateString;
+  };
 
-        return futureMonth;
+  omitZeroInFutureDate = (daysFromNow) => {
+    let date = this.getFutureDate(daysFromNow);
+
+    if (date[0] == 0) {
+      return date.substring(1, 2);
     }
+  };
 
-    getFutureDate() {
-        let futureDate = this.setDate().getDate();
+  getDatePickerYear() {
+    this.datepickerHeader
+      .find("span")
+      .first()
+      .should(($span) => {
+        expect($span.get(0).innerText.trim()).to.be.equal(
+          currentYear.toString()
+        );
+      });
+  }
 
-        return futureDate;
-    }
+  getDatePickerMonth() {
+    this.datepickerHeader
+      .find("span")
+      .last()
+      .should(($span) => {
+        expect($span.get(0).innerText.trim()).to.be.equal(currentMonth);
+      });
+  }
 
-    setFutureDate(date) {
-        this.futureDate = date;
-    }
-    
-    get taskBodyTabs() {
-        return cy.get('div[class="vs-c-modal__body-right el-col el-col-10"]').find('.el-tabs')
-    }
+  getDate(day, className, condition = "") {
+    return this.datepicker
+      .find("td")
+      .contains(day)
+      .should(`${condition}have.class`, className)
+      .and(`${condition}have.css`, "background-color", "rgb(78, 174, 147)");
+  }
 
-    get taskWorklog() {
-        return this.taskBodyTabs.find(".el-tabs__item").eq(1)
-    }
+  checkIfTodaysDateIsCorrect(className) {
+    return this.datepicker
+      .find("td")
+      .contains(yesterday)
+      .next()
+      .should("contain", "Today")
+      .and("have.class", className)
+      .and("have.css", "background-color", "rgb(78, 174, 147)");
+  }
 
-    get manageWorklog() {
-        return cy.get('button[title="Manage Worklog"]')
-    }
+  getfuturesDate(className, condition = "") {
+    return this.datepicker
+      .find("td")
+      .get(".available")
+      .contains(this.futureDate)
+      .should(`${condition}have.class`, className)
+      .and(`${condition}have.css`, "background-color", "rgb(78, 174, 147)");
+  }
 
-    get manageDateIcon() {
-        return cy.get('i[class="el-input__icon el-icon-date"')
-    }
+  datePickerSuccess() {
+    let day = yesterday;
+    // cy.log(yesterday)
+    // cy.log(tommorowsDate)
+    // cy.log(currentMonth)
+    // cy.log(currentYear)
 
-    get datepicker() {
-        return cy.get('div[class="el-picker-panel__content"]').find('tbody').eq(0);
-    }
-
-    get datepickerHeader() {
-        return cy.get('.el-date-picker__header')
-    }
-
-    get dateInputField() {
-        return cy.get('input[placeholder="Start date"]')
-    }
-
-    getDatePickerYear() {
-        this.datepickerHeader.find('span').first().should(($span) => {
-            expect($span.get(0).innerText.trim()).to.be.equal(currentYear.toString())
-        })
-    }
-
-    getDatePickerMonth() {
-        this.datepickerHeader.find('span').last().should(($span) => {
-            expect($span.get(0).innerText.trim()).to.be.equal(currentMonth)
-        })
-    }
-
-    getDate(day, className, condition = '') {
-        return this.datepicker.find('td').contains(day)
-            .should(`${condition}have.class`, className)
-            .and(`${condition}have.css`, 'background-color', 'rgb(78, 174, 147)')
-    }
-
-    checkIfTodaysDateIsCorrect(className) {
-        return this.datepicker.find('td').contains(yesterday).next().should('contain', 'Today')
-            .and('have.class', className)
-            .and('have.css', 'background-color', 'rgb(78, 174, 147)')
-    }
-
-    getfuturesDate(className, condition = '') {
-        let ker = this.getFutureDate();
-        cy.log(ker)
-        if (ker < 10)  {
-            ker.substring(1)};
-
-
-        return this.datepicker.find('td').get('.available').contains(this.futureDate)
-            .should(`${condition}have.class`, className)
-            .and(`${condition}have.css`, 'background-color', 'rgb(78, 174, 147)')
-    }
-
-    datePickerSuccess() {
-        let day = yesterday
-        // cy.log(yesterday)
-        // cy.log(tommorowsDate)
-        // cy.log(currentMonth)
-        // cy.log(currentYear)
-        
-        // cy.log(fiveDaysFromNow)
-        // cy.log(futureMonth)
-        // cy.log(futureDate)
-        this.taskWorklog.click();
-        this.manageWorklog.click();
-        this.manageDateIcon.click();
-        this.checkIfTodaysDateIsCorrect('available today current');
-        this.getDate(day, 'available current', 'not.').click()
-        this.getDate(day, 'available current')
-        this.manageDateIcon.click();
-        this.getDatePickerMonth()
-        this.getDatePickerYear()
-        // this.futuresDate(futureDate, 'available current', 'not.')
-        this.dateInputField.clear().type(this.getFutureDate() < 10 ? 0 : this.futureDate + ' ' +this.futureMonth + ' ' + currentYear)
-        this.manageDateIcon.click()
-        this.manageDateIcon.click()
-        this.getfuturesDate('available current')
-        
-
-    }
-
+    // cy.log(fiveDaysFromNow)
+    // cy.log(futureMonth)
+    // cy.log(futureDate)
+    this.taskWorklog.click();
+    this.manageWorklog.click();
+    this.manageDateIcon.click();
+    this.checkIfTodaysDateIsCorrect("available today current");
+    this.getDate(day, "available current", "not.").click();
+    this.getDate(day, "available current");
+    this.manageDateIcon.click();
+    this.getDatePickerMonth();
+    this.getDatePickerYear();
+    // this.futuresDate(futureDate, 'available current', 'not.')
+    this.dateInputField.clear().type();
+    this.manageDateIcon.click();
+    this.manageDateIcon.click();
+    this.getfuturesDate("available current");
+  }
 }
 export const datePicker = new DatePicker();
